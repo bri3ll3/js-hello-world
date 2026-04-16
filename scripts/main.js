9397f8d53dd6e06dab2ed64436f72f62662c496c
bri3ll3 <biacra2@alu.edu.gva.es>
2026-04-16 16:45:45 +0200

    primer commit

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+const miTitulo = document.querySelector("h1");
+miTitulo.textContent = "¡Hola mundo!";
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.addEventListener("click", () => {
+  setUserName();
+});
