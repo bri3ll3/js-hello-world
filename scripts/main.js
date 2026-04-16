a18066b497eb9906e7bc2f1467e507e83dd5647e
bri3ll3 <biacra2@alu.edu.gva.es>
2026-04-16 16:49:17 +0200

    refactor: traducí la petición del nombre a castellano

--- scripts/main.js
+++ scripts/main.js
@@ -5,12 +5,12 @@ let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
 
 function setUserName() {
-  let myName = prompt('Please enter your name.');
+  let myName = prompt('Introduce tu nombre.');
   if(!myName) {
     setUserName();
   } else {
     localStorage.setItem('name', myName);
-    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    myHeading.innerHTML = 'Mozilla es genial, ' + myName;
   }
 }
 
@@ -18,7 +18,7 @@ if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   let storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+  myHeading.innerHTML = 'Mozilla es genial, ' + storedName;
 }
 
 myButton.addEventListener("click", () => {
